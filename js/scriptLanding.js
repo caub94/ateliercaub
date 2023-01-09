f47d48ce4daff397aed4e3ded1c0f49a6831faae
caub94 <clara.beckers@yahoo.de>
2023-01-09 21:26:16 +0100

    Add files via upload

---
+++ js/scriptLanding.js
@@ -0,0 +1,19 @@
+//slideshow 
+//zoom in on logo when hover
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.querySelectorAll(".slideshowImage");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+/* still need to add Fade-In and Fade-Out */
